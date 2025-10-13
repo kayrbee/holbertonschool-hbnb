070d209b2df1a98ccdda9c58becff614be1f1f27
carlaciar <carlaciarr@gmail.com>
2025-10-13 14:30:28 +1100

    add review.py

---
+++ part2/hbnb/app/models/review.py
@@ -0,0 +1,16 @@
+from app.models.base_class import Base
+
+class Review(Base):
+    def __init__(self, rating: int, comment: str, place_id: str, user_id: str):
+        super().__init__()
+        self.rating = rating
+        self.comment = comment
+        self.place_id = place_id
+        self.user_id = user_id
+
+    # publish review method
+    def publish_review(self):
+        self.place_id.add_review()
+        # use the place id to look up place and access method in place to append review
+
+    # attach review to user.id
