ca3fa5ff47fa8c38e89d8bbba89d087aa0ee3d26
Dana Le <danadaole@gmail.com>
2025-10-15 13:53:05 +1100

    update the user part in app/services/facade file

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -6,6 +6,7 @@ from app.models.place import Place
 from app.models.amenity import Amenity
 from app.models.review import Review
 
+
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
@@ -13,16 +14,39 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
+    # User
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        """ create a new user (POST /users)"""
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user_by_id(self, user_id):
+        """ retrieve a user by ID (GET /users/<user_id>)"""
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        """ retrieve a user by email address (GET /users/email/<email_address>) """
+        return self.user_repo.get_by_attribute('email', email)
+
+    def get_all_users(self):
+        """ list all users that exist (GET /users)"""
+        return self.user_repo.get_all()
+
+    def put_user(self, user_id, new_data):
+        """ update existing user's information (PUT /users/<user_id>)"""
+        user = self.get_user(user_id)
+        if not user:
+            return None
+        user.update(new_data)
+        return user
 
     # Placeholder method for fetching a place by ID
+
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
-    
+
     # Amenity
     def create_amenity(self, amenity_data):
         """
@@ -30,7 +54,7 @@ class HBnBFacade:
         if it doesn't exist
         """
         amenity = Amenity(**amenity_data)
-        
+
         if not self.amenity_repo.get(amenity.id):
             self.amenity_repo.add(amenity)
         return amenity
@@ -54,7 +78,6 @@ class HBnBFacade:
         if not self.amenity_repo.get_all():
             raise ValueError("No Amenities found")
         return self.amenity_repo.get_all()
-            
 
     def update_amenity(self, amenity_id, amenity_data):
         """
