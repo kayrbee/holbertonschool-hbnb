e68051235226544e011840fd192f6a11fa50fe9e
harmel3056 <melh1988@hotmail.com>
2025-10-15 02:58:31 +0000

    Removed 'description' as this isn't modeled in the amenity instructions

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -4,7 +4,6 @@ from app.models.base_class import Base
 
 class Amenity(Base):
     amenities = []
-    def __init__(self, name: str, description: str):
+    def __init__(self, name: str):
         super().__init__()
         self.name = name
-        self.description = description
