9f54a357400b2764a9a00a6ffd91409b4d0909dc
carlaciar <carlaciarr@gmail.com>
2025-10-22 11:47:26 +1100

    edit test place

--- part2/tests/test_place.py
+++ part2/tests/test_place.py
@@ -70,6 +70,30 @@ class TestPlaceEndpoints(unittest.TestCase):
             "amenities": ["wifi, pool"]
         })
         self.assertEqual(response.status_code, 400)
+    
+    def test_get_all_places(self):
+        response = self.client.get('/api/v1/places')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_place_by_id(self):
+        create_response = self.client.post('/api/v1/places/', json={
+            "title": "Test Place",
+            "description": "Testing single place retrieval",
+            "price": 200,
+            "latitude": -37.8,
+            "longitude": 144.9,
+            "owner_id": "user123",
+            "amenities": ["wifi"]
+        })
+        self.assertEqual(create_response.status_code, 201)
+
+        # Get the ID from the response
+        place_id = create_response.get_json().get("id")
+
+        # Retrieve it by ID
+        response = self.client.get(f"/api/v1/places/{place_id}/")
+        self.assertEqual(response.status_code, 200)
+
 
 if __name__ == "__main__":
     unittest.main()
