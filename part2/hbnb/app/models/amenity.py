95569c205669089eba6c9b844b7e95b3d4c9d6db
harmel3056 <melh1988@hotmail.com>
2025-10-22 08:20:03 +0000

    Updated for JSON compatability

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,25 +1,28 @@
 from app.models.base_class import Base
 """
+Defines the Amenity model, including validation and serialization logic
 """
 
 class Amenity(Base):
     amenities = []
     def __init__(self, name: str):
         """
-        Checks input formatting to ensure entries are a string
-        and no more than 25 chars, then 
+        Initializes an Amenity instance, validating that the name is a string
+        and no longer than 50 characters
         """
         super().__init__()
 
-        # must be a string, less than 25 chars
+        # must be a string, less than 50 chars
         if not isinstance (name, str):
             raise TypeError("Amenity name must be a string")
-        if len(name) > 25:
-            raise ValueError("Amenity length cannot exceed 25 characters")
+        if len(name) > 50:
+            raise ValueError("Amenity length cannot exceed 50 characters")
         self.name = name
 
     def to_dict(self):
         return {
             "id": self.id,
-            "name": self.name
+            "name": self.name,
+            "created at": str(self.created_at),
+            "updated at": str(self.updated_at)
         }
