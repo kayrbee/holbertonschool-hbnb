6ef7141d1d63bc5d489aff26ee3c6ac99d21761c
Dana Le <danadaole@gmail.com>
2025-10-29 00:13:07 +1100

    move import namespaces to the top of file

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """
 """
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.users import api as users_ns
+
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
@@ -16,11 +21,6 @@ def create_app(config_class="config.DevelopmentConfig"):
 
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/api/v1/')
-    
-    from app.api.v1.users import api as users_ns
-    from app.api.v1.reviews import api as reviews_ns
-    from app.api.v1.places import api as places_ns
-    from app.api.v1.amenities import api as amenities_ns
 
     # register the endpoint namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
