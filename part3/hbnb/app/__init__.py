577f2c04a5687186e7cd165d27801d725f6fb2f7
Dana Le <danadaole@gmail.com>
2025-10-31 11:45:32 +1100

    remove py_cache to test

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -12,6 +12,11 @@ bcrypt = Bcrypt()
 # Instantiates a JWT manager class
 jwt = JWTManager()
 
+from app.api.v1.users import api as users_ns
+from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.auth import api as auth_ns
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -30,12 +35,6 @@ def create_app(config_class="config.DevelopmentConfig"):
 
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/api/v1/')
-
-    from app.api.v1.users import api as users_ns
-    from app.api.v1.reviews import api as reviews_ns
-    from app.api.v1.places import api as places_ns
-    from app.api.v1.amenities import api as amenities_ns
-    from app.api.v1.auth import api as auth_ns
 
     # register the endpoint namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
