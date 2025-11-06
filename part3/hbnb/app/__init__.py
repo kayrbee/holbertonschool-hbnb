92cd1058870bac7b6d254608378b381a17860036
harmel3056 <melh1988@hotmail.com>
2025-11-06 05:16:16 +0000

    Incorporates admin namespace

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,10 +1,13 @@
+
 #!/usr/bin/python3
 """
+Initialises a Flask app
 """
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 
 # Instantiates a password encryption class
 bcrypt = Bcrypt()
@@ -12,23 +15,23 @@ bcrypt = Bcrypt()
 # Instantiates a JWT manager class
 jwt = JWTManager()
 
+# Instanstiates a SQLAlchemy class
+db = SQLAlchemy()
+
 def create_app(config_class="config.DevelopmentConfig"):
-    
+
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     # Register bcrypt with the app instance
     bcrypt.init_app(app)
-    
     app.config.from_object(config_class)
+
     # Register the jwt middleware with the app instance
     jwt.init_app(app)
 
-    # Update the config with the JWT_SECRET_KEY
-    app.config.update(
-        TESTING=True,
-        SECRET_KEY='example-123'
-    )
+    # Register SQLAlchemy  with the app instance
+    db.init_app(app)
 
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/api/v1/')
@@ -39,6 +42,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     from app.api.v1.places import api as places_ns
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.auth import api as auth_ns
+    from app.api.v1.admin import api as admin_ns
 
     # register the endpoint namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
@@ -46,5 +50,6 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(auth_ns, path='/api/v1/auth')
-
+    api.add_namespace(admin_ns, path='/api/v1/admin')
+    
     return app
