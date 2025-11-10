00f9d4abc7a9e19b5c7dbb3c5520b848af406212
Katherine Beringer <github@katbee.au>
2025-11-10 22:53:12 +1100

    Refactor amenity_api tests to use test db | Extend test coverage of amenity_api to include all CRUD operations

--- part3/hbnb/tests/README.md
+++ part3/hbnb/tests/README.md
@@ -2,14 +2,35 @@
 
 ## Notes
 
-- add and configure test.db
-- convert all tests to unittest
+- convert all tests to `unittest` framework
 - use test_client in tests
+- improve coverage in all test files
 
 ### Decisions
 
 - chose unittest
-- chose to not implement an absolute path to db in config
+- chose to not implement an absolute path to db in config (yet)
+- chose to configure test.db to run in-memory (avoids need for tearDown() method)
+
+### Done
+
+MVP definition of done: test file uses `unittest`, tests in the file use a test db, and all tests in the file passed when run from the CLI
+
+Extension: make sure every test file covers all CRUD operations and defined error paths
+
+- add and configure test.db
+- test_auth_api.py
+- test_amenity_api.py
+
+### To do
+
+- test_amenity_class.py
+- test_place_api.py
+- test_place_class.py
+- test_review_api.py
+- test_review_class.py
+- test_user_api.py
+- test_user_class.py
 
 ## Prerequisites
 Ensure that the Flask application is running first
--- part3/hbnb/tests/test_amenity_api.py
+++ part3/hbnb/tests/test_amenity_api.py
@@ -1,23 +1,92 @@
 #!/usr/bin/python3
 import unittest
-from app import create_app
+import uuid
+from app import create_app, db
+from config import TestConfig
+from flask_jwt_extended import create_access_token
 
-class TestPlaceEndpoints(unittest.TestCase):
+
+def create_test_user(is_admin=False):
+    from app.models import User
+    unique_email = f"john.doe_{uuid.uuid4().hex}@example.com"
+    user = User(
+        first_name="Mary",
+        last_name="Admin",
+        email=unique_email,
+        password="password",
+        is_admin=is_admin
+    )
+    db.session.add(user)
+    db.session.commit()
+    return user
+
+
+def login(user):
+    access_token = create_access_token(
+        identity=str(user.id),
+        additional_claims={"is_admin": user.is_admin}
+    )
+
+    return access_token
+
+
+class TestAmenityEndpoints(unittest.TestCase):
 
     def setUp(self):
-        self.app = create_app()
+        """ Set up before each test """
+        # Create a fresh app and db before each test
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+        db.create_all()
+
+        self.app.testing = True
         self.client = self.app.test_client()
-   
-    def test_create_amenity(self):
-        response = self.client.post('/api/v1/amenities/', json={
-            "name": "Pool"
-        })
+
+        # Create an admin and a non-admin to test amenity creation
+        self.admin = create_test_user(is_admin=True)
+
+        # Create a valid admin jwt
+        self.admin_token = login(self.admin)
+
+        # Create auth header
+        self.admin_auth_header = {
+            "Authorization": f"Bearer {self.admin_token}"}
+
+        # Create a non-admin user to test protected endpoints
+        self.non_admin = create_test_user()
+
+        #  Create a valid non-admin jwt
+        self.user_token = login(self.non_admin)
+
+        # Create auth header
+        self.user_auth_header = {
+            "Authorization": f"Bearer {self.user_token}"}
+
+    def test_create_amenity_as_admin(self):
+        response = self.client.post(
+            '/api/v1/amenities/',
+            headers=self.admin_auth_header,
+            json={"name": "Pool"},
+            content_type="application/json"
+        )
         self.assertEqual(response.status_code, 201)
 
+    def test_create_amenity_as_non_admin(self):
+        response = self.client.post(
+            '/api/v1/amenities/',
+            headers=self.user_auth_header,
+            json={"name": "Pool"},
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 403)
+
     def test_create_amenity_too_long(self):
-        response = self.client.post('/api/v1/amenities/', json={
-            "name": "Aesop hand wash from the himalayas with plastic microbeads, which noone wants"
-        })
+        response = self.client.post(
+            '/api/v1/amenities/',
+            headers=self.admin_auth_header,
+            json={"name": "Aesop hand wash from the himalayas with plastic microbeads, which noone wants"})
         self.assertEqual(response.status_code, 400)
 
     def test_create_amenity_not_string(self):
@@ -26,13 +95,98 @@ class TestPlaceEndpoints(unittest.TestCase):
         })
         self.assertEqual(response.status_code, 400)
 
-    #   TO DO: empty handling
-    # def test_create_empty_amenity(self):
-    #     response = self.client.post('/api/v1/amenities/', json={
-    #         "name": ""
-    #     })
-    #     self.assertEqual(response.status_code, 400)
+    def test_create_amenity_empty_name(self):
+        response = self.client.post(
+            '/api/v1/amenities/',
+            headers=self.admin_auth_header,
+            json={"name": None})
+        self.assertEqual(response.status_code, 400)
+
+    def test_get_all_amenities(self):
+        # First, create an amenity to return
+        self.client.post(
+            '/api/v1/amenities/',
+            headers=self.admin_auth_header,
+            json={"name": "Pool"},
+            content_type="application/json"
+        )
+
+        response = self.client.get('/api/v1/amenities/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_amenity_by_id(self):
+        # First, create an amenity to return
+        response = self.client.post(
+            '/api/v1/amenities/',
+            headers=self.admin_auth_header,
+            json={"name": "Pool"},
+            content_type="application/json"
+        )
+        amenity = response.get_json()
+        amenity_id = amenity["id"]
+
+        # Pass the amenity_id to the endpoint for testing
+        response = self.client.get(f'/api/v1/amenities/{amenity_id}')
+        self.assertEqual(response.status_code, 200)
+
+    def test_modify_amenity_as_admin(self):
+        # First, create an amenity to modify
+        response = self.client.post(
+            '/api/v1/amenities/',
+            headers=self.admin_auth_header,
+            json={"name": "Pool"},
+            content_type="application/json"
+        )
+        amenity = response.get_json()
+        amenity_id = amenity["id"]
+
+        # Pass the amenity_id to the endpoint for testing
+        response = self.client.put(
+            f'/api/v1/amenities/{amenity_id}',
+            headers=self.admin_auth_header,
+            json={"name": "Spa"},
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.get_json()
+        self.assertEqual(data["name"], "Spa")
+
+    def test_delete_amenity_as_admin(self):
+        # First, create an amenity to delete
+        response = self.client.post(
+            '/api/v1/amenities/',
+            headers=self.admin_auth_header,
+            json={"name": "Pool"},
+            content_type="application/json"
+        )
+        amenity = response.get_json()
+        amenity_id = amenity["id"]
+
+        # Pass the amenity_id to the endpoint for testing
+        response = self.client.delete(
+            f'/api/v1/amenities/{amenity_id}',
+            headers=self.admin_auth_header
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_amenity_as_non_admin(self):
+        # First, create an amenity to delete
+        response = self.client.post(
+            '/api/v1/amenities/',
+            headers=self.admin_auth_header,
+            json={"name": "Pool"},
+            content_type="application/json"
+        )
+        amenity = response.get_json()
+        amenity_id = amenity["id"]
+
+        # Pass the amenity_id to the endpoint for testing
+        response = self.client.delete(
+            f'/api/v1/amenities/{amenity_id}',
+            headers=self.user_auth_header
+        )
+        self.assertEqual(response.status_code, 403)
+
 
 if __name__ == "__main__":
     unittest.main()
-
