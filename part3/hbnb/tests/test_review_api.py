1cae1f455deebee8e03b1221de3700b0ce07f2a6
Katherine Beringer <github@katbee.au>
2025-11-12 18:09:58 +1100

    Refactor review_api tests & get them passing

--- part3/hbnb/tests/test_review_api.py
+++ part3/hbnb/tests/test_review_api.py
@@ -1,232 +1,173 @@
-# <-- Commented out because Kat hasn't fixed these yet -->
-
-# import unittest
-# import json
-# import uuid
-# from app import create_app
-
-
-# class TestReviewEndpoints(unittest.TestCase):
-
-#     def setUp(self):
-#         self.app = create_app()
-#         self.app.testing = True
-#         self.client = self.app.test_client()
-
-#         # Create test data
-#         # Pre-conditions for create_review:
-#         #  - User must exist
-#         #  - User cannot review own place
-
-#         # Create a place owner id
-#         owner_email = f"test_{uuid.uuid4().hex}@example.com"
-#         owner_password = "password123"
-#         owner_response = self.client.post(
-#             "/api/v1/users/",
-#             json={
-#                 "first_name": "Mary",
-#                 "last_name": "Owner",
-#                 "email": owner_email,
-#                 "password": owner_password
-#             }
-#         )
-#         self.assertEqual(owner_response.status_code, 201)
-
-#         # Save owner details
-#         self.owner_id = owner_response.get_json()["id"]
-#         self.owner_jwt_token = self.get_jwt_token(owner_email, owner_password)
-#         self.owner_auth_headers = {
-#             "Authorization": f"Bearer {self.owner_jwt_token}"}
-
-#         # Create a valid user_id who can submit a review
-#         reviewer_email = f"test_{uuid.uuid4().hex}@example.com"
-#         reviewer_password = "password123"
-#         reviewer_response = self.client.post(
-#             "/api/v1/users/",
-#             json={
-#                 "first_name": "John",
-#                 "last_name": "Reviewer",
-#                 "email": reviewer_email,
-#                 "password": reviewer_password
-#             }
-#         )
-#         self.assertEqual(reviewer_response.status_code, 201)
-
-#         # Set self.user_attributes for use in tests
-#         self.reviewer_id = reviewer_response.get_json()["id"]
-#         self.reviewer_email = reviewer_email
-#         self.reviewer_password = reviewer_password
-
-#         # Set self.authorization_attributes for use in tests
-#         self.jwt_token = self.get_jwt_token(
-#             self.reviewer_email, self.reviewer_password)
-#         self.auth_headers = {"Authorization": f"Bearer {self.jwt_token}"}
-
-#         # Pre-condition for create_review - Place must exist
-#         # Create a valid place_id
-#         place_response = self.client.post(
-#             "/api/v1/places/",
-#             headers=self.owner_auth_headers,
-#             json={
-#                 "title": "Test Place",
-#                 "description": "Test description",
-#                 "latitude": 0.0,
-#                 "longitude": 0.0,
-#                 "price": 100,
-#                 "owner_id": self.owner_id,
-#                 "reviews": [],
-#                 "amenities": []
-#             }
-#         )
-#         self.assertEqual(place_response.status_code, 201)
-#         self.place_id = place_response.get_json()['id']
-#         # Dummy review data (valid)
-#         self.review_data = {
-#             "text": "A lovely place",
-#             "rating": 5,
-#             "user": self.reviewer_id,
-#             "place": self.place_id
-#         }
-
-#     # helper method - create a valid jwt token
-#     def get_jwt_token(self, user_email, user_password):
-#         response = self.client.post(
-#             "/api/v1/auth/login",
-#             json={"email": user_email, "password": user_password},
-#             content_type="application/json"
-#         )
-#         self.assertEqual(response.status_code, 200)
-#         data = response.get_json()
-#         return data["access_token"]
-
-#     # Commented out because it's consistently failing
-#     # And I haven't fixed it yet
-#     # def test_get_reviews_empty_list(self):
-#     #     """ Get reviews should return an empty list when there are no reviews
-#     #     Note: currently failing because test run order is not guaranteed
-#     #     """
-#     #     response = self.client.get('/api/v1/reviews/')
-#     #     self.assertEqual(response.status_code, 200)
-#     #     data = response.get_json()
-#     #     self.assertIsInstance(data, list)
-#     #     self.assertEqual(len(data), 0)
-
-#     def test_create_review_success(self):
-#         """ POST /api/v1/reviews/ should create a review """
-#         response = self.client.post(
-#             "/api/v1/reviews/",
-#             headers=self.auth_headers,
-#             json=self.review_data,
-#             content_type="application/json"
-#         )
-#         # Handy debugging statement!
-#         # print(response.status_code, response.get_json())
-#         self.assertEqual(response.status_code, 201)
-#         data = response.get_json()
-#         self.assertIn("id", data)
-#         self.assertEqual(data["text"], self.review_data["text"])
-#         self.review_id = data["id"]
-
-#     def test_create_review_with_missing_rating(self):
-#         """POST /api/v1/reviews should fail with missing field"""
-#         bad_data = self.review_data.copy()
-#         del bad_data["rating"]
-
-#         response = self.client.post(
-#             "/api/v1/reviews/",
-#             headers=self.auth_headers,
-#             json=bad_data,
-#             content_type="application/json"
-#         )
-#         self.assertEqual(response.status_code, 400)
-#         data = response.get_json()
-#         self.assertIn("errors", data)
-#         self.assertIn("rating", data["errors"])
-
-#     def test_get_all_reviews_with_one_review_in_list(self):
-#         """GET /api/v1/reviews/ should return a list of reviews"""
-
-#         response = self.client.get("/api/v1/reviews/")
-#         self.assertEqual(response.status_code, 200)
-#         data = response.get_json()
-#         self.assertIsInstance(data, list)
-#         self.assertGreaterEqual(len(data), 1)
-
-#     def test_get_review_by_id(self):
-#         """GET /api/v1/reviews/<id> should return the review <id> """
-#         # Pre-condition - create review & save its id
-#         post_resp = self.client.post(
-#             "/api/v1/reviews/",
-#             headers=self.auth_headers,
-#             json=self.review_data,
-#             content_type="application/json"
-#         )
-#         review_id = post_resp.get_json()['id']
-
-#         get_resp = self.client.get(f"/api/v1/reviews/{review_id}")
-#         self.assertEqual(get_resp.status_code, 200)
-#         self.assertEqual(get_resp.get_json()["id"], review_id)
-
-#     def test_update_review(self):
-#         """PUT /api/v1/reviews/<id> should update the review"""
-#         # Create a review
-#         post_resp = self.client.post(
-#             "/api/v1/reviews/",
-#             headers=self.auth_headers,
-#             json=self.review_data,
-#             content_type="application/json"
-#         )
-#         review_id = post_resp.get_json()["id"]
-
-#         updated_data = self.review_data.copy()
-#         updated_data["rating"] = 1
-#         updated_data["text"] = "Updated review text"
-
-#         put_resp = self.client.put(
-#             f"/api/v1/reviews/{review_id}",
-#             headers=self.auth_headers,
-#             json=updated_data,
-#             content_type="application/json"
-#         )
-#         self.assertEqual(put_resp.status_code, 200)
-#         self.assertEqual(put_resp.get_json()["text"], "Updated review text")
-#         self.assertEqual(put_resp.get_json()["rating"], 1)
-
-#     def test_delete_review(self):
-#         """DELETE /api/v1/reviews/<id> should delete the review <id>"""
-#         post_resp = self.client.post(
-#             "/api/v1/reviews/",
-#             headers=self.auth_headers,
-#             json=self.review_data,
-#             content_type="application/json"
-#         )
-#         review_id = post_resp.get_json()["id"]
-
-#         del_resp = self.client.delete(
-#             f"/api/v1/reviews/{review_id}",
-#             headers=self.auth_headers
-#         )
-#         self.assertEqual(del_resp.status_code, 200)
-
-#         get_resp = self.client.get(f"/api/v1/reviews/{review_id}")
-#         self.assertEqual(get_resp.status_code, 404)
-
-#     def test_get_reviews_by_place(self):
-#         """GET /api/v1/reviews/places/<place_id>/reviews should return reviews"""
-#         self.client.post(
-#             "/api/v1/reviews/",
-#             headers=self.auth_headers,
-#             json=self.review_data,
-#             content_type="application/json"
-#         )
-
-#         response = self.client.get(
-#             f"/api/v1/reviews/places/{self.review_data['place']}/reviews")
-#         self.assertEqual(response.status_code, 200)
-#         data = response.get_json()
-#         self.assertIsInstance(data, list)
-#         self.assertGreaterEqual(len(data), 1)
+import unittest
+import json
+import uuid
+from app import create_app, db
+from config import TestConfig
+import tests.helper_methods as setup
+from flask_jwt_extended import decode_token
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+class TestReviewEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        """ Set up before each test """
+        # Create a fresh app and db before each test
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+        db.create_all()
+
+        self.app.testing = True
+        self.client = self.app.test_client()
+
+        # Create a reviewer & jwt token
+        self.reviewer = setup.create_test_user()
+        self.reviewer_id = self.reviewer.id
+        self.reviewer_token = setup.login(self.reviewer)
+        self.reviewer_auth_header = {
+            "Authorization": f"Bearer {self.reviewer_token}"}
+
+        # Create a place owner
+        self.owner = setup.create_test_user()
+        self.owner_id = self.owner.id
+        self.owner_token = setup.login(self.owner)
+        self.owner_auth_headers = {
+            "Authorization": f"Bearer {self.owner_token}"}
+
+        # Create a place to review
+        self.place = setup.create_place(self.owner_id)
+        self.place_id = self.place.id
+
+        # Dummy review data (valid)
+        self.review_data = {
+            "text": "A lovely place",
+            "rating": 5,
+            "user": self.reviewer_id,
+            "place": self.place_id
+        }
+
+    def test_get_reviews_empty_list(self):
+        """ Get reviews should return an empty list when there are no reviews
+        """
+        response = self.client.get('/api/v1/reviews/')
+        self.assertEqual(response.status_code, 200)
+        data = response.get_json()
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 0)
+
+    def test_create_review_success(self):
+        """ POST /api/v1/reviews/ should create a review - reviewer cannot be owner """
+        response = self.client.post(
+            "/api/v1/reviews/",
+            headers=self.reviewer_auth_header,
+            json=self.review_data,
+            content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 201)
+        data = response.get_json()
+        self.assertIn("id", data)
+        self.assertEqual(data["text"], self.review_data["text"])
+        self.review_id = data["id"]
+
+    def test_create_review_with_missing_rating(self):
+        """POST /api/v1/reviews should fail with missing field"""
+        self.review_data["rating"] = None
+
+        response = self.client.post(
+            "/api/v1/reviews/",
+            headers=self.reviewer_auth_header,
+            json=self.review_data,
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+        data = response.get_json()
+        self.assertIn("errors", data)
+        self.assertIn("rating", data["errors"])
+
+    def test_get_all_reviews_with_one_review_in_list(self):
+        """GET /api/v1/reviews/ should return a list of reviews"""
+        # Create a review to return
+        review = setup.create_review(self.place_id, self.reviewer_id)
+
+        response = self.client.get("/api/v1/reviews/")
+        self.assertEqual(response.status_code, 200)
+        data = response.get_json()
+
+        # Check that there's data in the list
+        self.assertIsInstance(data, list)
+        self.assertGreaterEqual(len(data), 1)
+
+        #  Check that the review_id is in the returned data
+        review_texts = [r["id"] for r in data]
+        self.assertIn(review.id, review_texts)
+
+    def test_get_review_by_id(self):
+        """GET /api/v1/reviews/<id> should return the review <id> """
+        # Pre-condition - create review & save its id
+        review = setup.create_review(self.place_id, self.reviewer_id)
+
+        get_resp = self.client.get(f"/api/v1/reviews/{review.id}")
+        self.assertEqual(get_resp.status_code, 200)
+        self.assertEqual(get_resp.get_json()["id"], review.id)
+
+    def test_update_review_as_reviewer(self):
+        """PUT /api/v1/reviews/<id> should update the review"""
+        # Create a review
+        review = setup.create_review(self.place_id, self.reviewer_id)
+        review_id = review.id
+
+        # Update the review data
+        self.review_data["rating"] = 1
+        self.review_data["text"] = "Updated review text"
+        # print("Created review:", review.to_dict())
+        # print("PUT payload:", self.review_data)
+        # print("Current user from auth header:",
+        #       decode_token(self.reviewer_token)["sub"])
+        # print("review_id", review_id, "user: ",
+        #       self.reviewer_id, "place:", self.place_id)
+
+        response = self.client.put(
+            f"/api/v1/reviews/{review_id}",
+            headers=self.reviewer_auth_header,
+            json=self.review_data,
+            content_type="application/json"
+        )
+        # Handy debugging statement!
+        print(response.status_code, response.get_json())
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json()["text"], "Updated review text")
+        self.assertEqual(response.get_json()["rating"], 1)
+
+    def test_delete_review(self):
+        """DELETE /api/v1/reviews/<id> should delete the review <id>"""
+        # create a review
+        review = setup.create_review(self.place_id, self.reviewer_id)
+        review_id = review.id
+
+        del_resp = self.client.delete(
+            f"/api/v1/reviews/{review_id}",
+            headers=self.reviewer_auth_header,
+        )
+        self.assertEqual(del_resp.status_code, 200)
+
+        get_resp = self.client.get(f"/api/v1/reviews/{review_id}")
+        self.assertEqual(get_resp.status_code, 404)
+
+    def test_get_reviews_by_place(self):
+        """GET /api/v1/reviews/places/<place_id>/reviews should return reviews"""
+        review = setup.create_review(self.place_id, self.reviewer_id)
+        print(review)
+
+        response = self.client.get(
+            f"/api/v1/reviews/places/{self.place_id}/reviews")
+        self.assertEqual(response.status_code, 200)
+        data = response.get_json()
+        self.assertIsInstance(data, list)
+        self.assertGreaterEqual(len(data), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
